fc87da05d0fd2e91b2bb6448ad57480f1bd33b1e
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-02-11 19:43:45 -0300

    Cleaning the code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
-import Header from '../src/components/Header'
-
 export default function Home() {
   return (
     <>
-      <Header />
-      <h1>Home</h1>
     </>
   )
 }
---
+++ pages/main/index.tsx
@@ -0,0 +1,7 @@
+import Header from '../../src/components/Header'
+
+export default function Main() {
+    return (
+        <Header />
+    )
+}
