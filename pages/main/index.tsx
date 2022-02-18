5fe5d4f3fe6085f7d0366d6d3cb39e79ea58e29a
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-02-18 17:28:56 -0300

    Create Carousel component

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,7 +1,11 @@
+import Carousel from '../../src/components/Carousel'
 import Header from '../../src/components/Header'
 
 export default function Main() {
     return (
-        <Header />
+        <>
+            <Header />
+            <Carousel />
+        </>
     )
 }
