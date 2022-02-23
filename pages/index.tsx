311493d7905f883ad9197c9b2f9278b96e1ca2fe
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-02-23 19:28:29 -0300

    Create Categories component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Main from '../pages/main'
 import BannerHome from '../src/components/BannerHome'
+import Categories from '../src/components/Categories'
 
 export default function Home() {
   return (
     <>
       <Main />
       <BannerHome />
+      
     </>
   )
 }
